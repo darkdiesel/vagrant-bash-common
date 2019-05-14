ab06d9e37867f024baac612d62dd2a7827e1e3d0
Igor Peshkov <igor.peshkov@gmail.com>
2019-05-14 11:33:18 +0300

    #8 - Setup swap for removing problem with installation composer packages.

--- vagrant/scripts/ubuntu/common/composer.sh
+++ vagrant/scripts/ubuntu/common/composer.sh
@@ -12,3 +12,10 @@ sudo mv composer.phar /usr/local/bin/composer > /dev/null 2>&1
 
 #sudo cp /vagrant/composer/auth.json /root/.composer/auth.json > /dev/null 2>&1
 log_end_msg 0
+
+log_begin_msg "Setup swap for correct packages installation"
+# see for more info https://getcomposer.org/doc/articles/troubleshooting.md#proc-open-fork-failed-errors
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024 > /dev/null 2>&1
+sudo /sbin/mkswap /var/swap.1 > /dev/null 2>&1
+sudo /sbin/swapon /var/swap.1 > /dev/null 2>&1
+log_end_msg 0
