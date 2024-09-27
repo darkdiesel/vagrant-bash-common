75a84ff1f2050a60aff4eff79d259751ec4e44fd
Igor Peshkov <igor.peshkov@gmail.com>
2024-09-27 11:56:36 +0300

    Update nvm and example settings

--- vagrant/scripts/ubuntu/common/nvm.sh
+++ vagrant/scripts/ubuntu/common/nvm.sh
@@ -6,12 +6,15 @@ if [ $(nvm --help 2>/dev/null | grep -c "Node Version Manager") -eq 0 ]; then
     if [ -n "$PACKAGES__NVM__VERSION" ]; then
         eval NVM_VERSION='$PACKAGES__NVM__VERSION';
     else
-        NVM_VERSION='0.39.7'
+        NVM_VERSION='0.40.1'
     fi;
 
     log_begin_msg "Installing nvm for root user"
 
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v"${NVM_VERSION}"/install.sh > /dev/null 2>&1 |  bash  > /dev/null 2>&1
+
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
     if [[ $? > 0 ]]; then
         log_end_msg 1
@@ -24,6 +27,9 @@ if [ $(nvm --help 2>/dev/null | grep -c "Node Version Manager") -eq 0 ]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v"${NVM_VERSION}"/install.sh > /dev/null 2>&1 |  sudo -H -u vagrant bash > /dev/null 2>&1
     sudo -H -u vagrant bash -c 'source ~/.bashrc' > /dev/null 2>&1
 
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
     source ~/.nvm/nvm.sh
     source ~/.bashrc
     source ~/.profile
@@ -35,15 +41,4 @@ if [ $(nvm --help 2>/dev/null | grep -c "Node Version Manager") -eq 0 ]; then
     fi
 else
     log_progress_msg "nvm already installed"
-fi
-
-if [ ! -d ~/.nvm ]; then
-
-  curl https://raw.githubusercontent.com/creationix/nvm/v0.11.1/install.sh | bash
-  source ~/.nvm/nvm.sh
-  source ~/.profile
-  source ~/.bashrc
-  nvm install 5.0
-  npm install
-  npm run front
 fi
