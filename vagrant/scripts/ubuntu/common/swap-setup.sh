d0aa3b95733d8af10dc285d4f49cedfd6532dcc2
Igor Peshkov <igor.peshkov@gmail.com>
2025-10-13 11:39:09 +0300

    Add swap-setup script

---
+++ vagrant/scripts/ubuntu/common/swap-setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+log_begin_msg "Setup swap"
+# see for more info https://getcomposer.org/doc/articles/troubleshooting.md#proc-open-fork-failed-errors
+sudo /bin/dd if=/dev/zero of=/swapfile bs=1M count=1024 > /dev/null 2>&1
+sudo /bin/chmod 0600 /swapfile
+sudo /sbin/mkswap /swapfile > /dev/null 2>&1
+sudo /sbin/swapon /swapfile > /dev/null 2>&1
+
+SWAP_PERMANENT="/swapfile   none    swap    sw    0   0"
+
+if grep -vq "${SWAP_PERMANENT}" /etc/fstab; then
+    echo "${SWAP_PERMANENT}" | sudo tee -a /etc/fstab
+fi
+log_end_msg 0
